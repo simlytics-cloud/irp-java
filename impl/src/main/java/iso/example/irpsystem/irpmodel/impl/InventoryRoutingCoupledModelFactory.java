a90d854bdb23b1c8f9407bea710935ad0851e750
robert.kewley <rob@simlytics.clouod>
2026-02-06 18:13:51 -0500

    Add `InventoryRoutingCoupledModelFactory` to enable coupled model creation with Kafka integration support

---
+++ impl/src/main/java/iso/example/irpsystem/irpmodel/impl/InventoryRoutingCoupledModelFactory.java
@@ -0,0 +1,54 @@
+package iso.example.irpsystem.irpmodel.impl;
+
+import java.util.List;
+
+import org.apache.pekko.actor.typed.ActorRef;
+import org.apache.pekko.actor.typed.javadsl.ActorContext;
+
+import com.typesafe.config.Config;
+
+import devs.CoupledModelFactory;
+import devs.PDevsCouplings;
+import devs.SimulatorProvider;
+import devs.iso.DevsMessage;
+import devs.iso.time.LongSimTime;
+import devs.proxy.KafkaReceiver;
+
+public class InventoryRoutingCoupledModelFactory  extends CoupledModelFactory<LongSimTime> {
+
+    protected Config kafkaConsumerConfig = null;
+    protected String kafkaTopic = "";
+
+    public InventoryRoutingCoupledModelFactory(String modelIdentifier,
+            List<SimulatorProvider<LongSimTime>> simulatorProviders, PDevsCouplings couplings) {
+        super(modelIdentifier, simulatorProviders, couplings);
+    }
+
+    public InventoryRoutingCoupledModelFactory(String modelIdentifier,
+            List<SimulatorProvider<LongSimTime>> simulatorProviders, PDevsCouplings couplings,
+            Config kafkaConsumerConfig,
+            String kafkaTopic) {
+        super(modelIdentifier, simulatorProviders, couplings);
+        this.kafkaConsumerConfig = kafkaConsumerConfig;
+    }
+
+    public InventoryRoutingCoupledModelFactory(String modelIdentifier,
+            List<SimulatorProvider<LongSimTime>> simulatorProviders, PDevsCouplings couplings,
+            List<String> loggingModels) {
+        super(modelIdentifier, simulatorProviders, couplings, loggingModels);
+    }
+
+    @Override
+    public ActorRef<DevsMessage> provideSimulator(ActorContext<DevsMessage> context, LongSimTime initialTime) {
+        // TODO Auto-generated method stub
+        ActorRef<DevsMessage> coordinator = super.provideSimulator(context, initialTime);
+        if (kafkaConsumerConfig != null) {
+            context.spawn(KafkaReceiver.create(coordinator, null, modelIdentifier, kafkaConsumerConfig, 
+                "irp-system"), "irpsystemproxy");
+        }
+        return coordinator;
+    }
+
+    
+
+}
